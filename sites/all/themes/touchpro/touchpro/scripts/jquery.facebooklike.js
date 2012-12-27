31b64633dbe8abcb139493ecacebe10cd5dc721f
Jeppe Bergmann Hamming <jeppe.hamming@gmail.com>
2012-12-27 14:55:01 +0100

    Moved the facebook width manipulation to the touchpro theme

---
+++ sites/all/themes/touchpro/touchpro/scripts/jquery.facebooklike.js
@@ -0,0 +1,11 @@
+jQuery(window).load(function () {
+	//Manipulate facebook box width. Since the iframe has no ID i need to match on the source attribute
+	var facebookIframe = new Array();
+	facebookIframe = jQuery('iframe');
+	for(var i=0;i<facebookIframe.length;i++){
+		var facebookMatch=facebookIframe[i].src.match(/facebook/g);
+		if (facebookMatch !== undefined) {
+			facebookIframe[i].setAttribute("style","border:none; overflow:hidden; width:100%; height:690px;");
+		}
+	}
+});
